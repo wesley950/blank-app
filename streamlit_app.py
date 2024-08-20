5e4c0e14abe1521d9d989ba5b7f49682c51f7647
Wesley <82453415+wesley950@users.noreply.github.com>
2024-08-20 13:44:39 +0000

    isuehfisuheugshrg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,139 @@
+import json, hmac
+import openai
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+from datetime import datetime
+from uuid import uuid4
+
+
+SYSTEM_PROMPT = """You are a chatbot put on a gym website. You can use the following tools:
+
+- store_snapchat_username: Store Snapchat username provided by user during chat.
+
+Your job is to help onboard new clients and help them by providing info.
+The gym is open in the city of San Francisco. You can use the following information:
+
+Open hours: 08:00 - 22:00 Monday - Friday
+Subscription fee: $5.00
+
+If the user shows interest in joining the gym, you can provide more info,
+but ask for their Snapchat username so you can send details to them."""
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "store_snapchat_username",
+            "description": "Store username provided by user during chat.",
+            "parameters": {
+                "type": "object",
+                "properties": {"username": {"type": "string"}},
+                "required": ["username"],
+            },
+        },
+    }
+]
+
+
+def _store_snapchat_username(username: str):
+    """Store username provided by user during chat."""
+    st.warning(f"Your Snapchat username is: {username}")
+
+    return {
+        "id": uuid4().hex,
+        "username": username,
+        "opted_in": datetime.now().isoformat(),
+        "success": True,
+    }
+
+
+def check_password():
+    def password_entered():
+        if hmac.compare_digest(
+            st.session_state["password"], "78PXjylASfjELTRvOhXCFt8YY5kf7rNn"
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if st.session_state.get("password_correct", False):
+        return True
+
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("Password incorrect")
+
+    return False
+
+
+if __name__ == "__main__":
+    st.title("Snapchat Demo")
+    st.caption("Streamlit app for demo purposes")
+
+    if not check_password():
+        st.stop()
+
+    with st.sidebar:
+        openai_api_key = st.text_input(
+            "OpenAI API key", type="password", key="chatbot_api_key"
+        )
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "assistant", "content": "Hello, how can I help?"},
+        ]
+
+    for message in st.session_state["messages"]:
+        info = dict(message)
+        st.chat_message(info["role"]).write(info["content"])
+
+    if prompt := st.chat_input():
+        if not openai_api_key:
+            st.error("OpenAI API key is required")
+
+        st.session_state["messages"].append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+
+        response = (
+            openai.chat.completions.create(
+                model="gpt-4o",
+                messages=st.session_state["messages"],
+                tools=tools,
+                openai_api_key=openai_api_key,
+            )
+            .choices[0]
+            .message
+        )
+        st.session_state["messages"].append(response)
+        if response.content:
+            st.chat_message("assistant").write(response.content)
+
+        if response.tool_calls:
+            for tool_call in response.tool_calls:
+                fn_name = tool_call.function.name
+                if fn_name == "store_snapchat_username":
+                    args = json.loads(tool_call.function.arguments)
+                    user = _store_snapchat_username(args["username"])
+                    st.session_state["messages"].append(
+                        {
+                            "role": "tool",
+                            "content": json.dumps(user),
+                            "tool_call_id": tool_call.id,
+                        }
+                    )
+
+                    new_response = (
+                        openai.chat.completions.create(
+                            model="gpt-4o",
+                            messages=st.session_state["messages"],
+                            openai_api_key=openai_api_key,
+                        )
+                        .choices[0]
+                        .message
+                    )
+                    st.session_state["messages"].append(new_response)
+                    st.chat_message("assistant").write(new_response.content)
